aaadb8e9fbbaa93a3db0e6080a4077d25a43c1d9
student <student@gwcadmins-MacBook-Pro.local>
2019-07-18 09:53:23 -0500

    initial commit

---
+++ myScript.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>About me</h2>
+
+
+<p id="demo" style="display:none">about me </p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
+
+</body>
+</html>
